8fddd739a8baff6949d4b955e6944964881a72ac
Administrator <Administrator@172.20.0.105>
2022-07-21 11:49:37 +0530

    initial commit

---
+++ SpringBootDataJpaHIbernateEmployeeCrudRest/src/test/java/com/onmobile/empmanagement/SpringBootJpaHIbernateEmployeeCrudRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onmobile.empmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaHIbernateEmployeeCrudRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
